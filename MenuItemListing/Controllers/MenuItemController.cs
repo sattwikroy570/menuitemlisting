63d67ebd46d981faf86772852c51fb8a71a2de72
sattwikroy570 <sattwikroy@hotmail.com>
2022-06-13 17:14:35 +0530

    e

--- MenuItemListing/Controllers/MenuItemController.cs
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using MenuItemListing.Models;
 
-[Authorize]
+
 namespace MenuItemListing.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
+    [Authorize]
     public class MenuItemController : ControllerBase
     {
         [HttpGet]
